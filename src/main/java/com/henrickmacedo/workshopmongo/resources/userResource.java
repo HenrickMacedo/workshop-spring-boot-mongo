b2e6c0ee6fbb36df1425e6edcafcbad0c70562ea
Henrick Macedo <henricksou@gmail.com>
2024-09-11 11:22:04 -0300

    Endpoint para retornar os posts de um Usuario

--- src/main/java/com/henrickmacedo/workshopmongo/resources/userResource.java
+++ src/main/java/com/henrickmacedo/workshopmongo/resources/userResource.java
@@ -6,9 +6,17 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.henrickmacedo.workshopmongo.domain.Post;
 import com.henrickmacedo.workshopmongo.domain.User;
 import com.henrickmacedo.workshopmongo.dto.UserDTO;
 import com.henrickmacedo.workshopmongo.services.UserService;
@@ -25,15 +33,15 @@ public class UserResource {
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDto);
-		
+		return ResponseEntity.ok().body(listDto);	
 	}
+	
 	@GetMapping(value="/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
-		
 	}
+	
 	@PostMapping
 	public ResponseEntity<Void> Insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
@@ -55,4 +63,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
