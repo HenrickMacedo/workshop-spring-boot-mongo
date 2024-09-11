8e5215b2a9fc890f56e84dc1b205d682af690260
Henrick Macedo <henricksou@gmail.com>
2024-09-11 11:15:48 -0300

    Referenciando os posts do usuario

--- src/main/java/com/henrickmacedo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/henrickmacedo/workshopmongo/config/Instantiation.java
@@ -41,9 +41,10 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, null, "Partiu Viagem", "Vou viajar para São Paulo, Abraços!!", new AuthorDTO(maria));
 		Post post2 = new Post(null, null, "Bom dia", "Acordei Feliz hoje!!", new AuthorDTO(maria));
 		
-		
-
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
